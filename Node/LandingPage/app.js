727ad7352d56e60461fd991dfeafa2b2b45686eb
Nishant Aggarwal <nish8192@gmail.com>
2017-04-04 14:31:54 -0400

    Finished CarsCats in Node

--- Node/LandingPage/app.js
+++ Node/LandingPage/app.js
@@ -14,7 +14,20 @@ var server = http.createServer(function (request, response){
             response.end(); // finished!
         });
     }
-    // request didn't match anything:
+    else if(request.url === '/ninjas') {
+        fs.readFile('ninjas.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    }
+    else if(request.url === '/dojos/new') {
+        fs.readFile('dojos.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    }
     else {
         response.writeHead(404);
         response.end('File not found!!!');
