eb5956a4066c7bbeb2be31baa4e24bbbe20189a6
Nishant Aggarwal <nish8192@gmail.com>
2017-04-05 15:28:24 -0400

    Finished basic dojo app

---
+++ MongoDB/basic_mongoose_app/server.js
@@ -0,0 +1,72 @@
+// require express
+var express = require("express");
+// path module
+var path = require("path");
+// create the express app
+var app = express();
+
+//require mongoose
+var mongoose = require('mongoose');
+//connect to mongoose server
+mongoose.connect('mongodb://localhost/basic_mongoose_app');
+mongoose.Promise = global.Promise;
+//basic user schema (use as blueprint)
+var UserSchema = new mongoose.Schema({
+    name: String,
+    age: Number
+})
+mongoose.model('User', UserSchema); // We are setting this Schema in our Models as 'User'
+var User = mongoose.model('User')
+
+var bodyParser = require('body-parser');
+// use it!
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// static content
+app.use(express.static(path.join(__dirname, "./static")));
+// setting up ejs and our views folder
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+
+// root route to render the index.ejs view
+app.get('/', function(req, res) {
+    User.find({}, function(err, users){
+        if(err){
+            console.log(err);
+        }
+        else if(users){
+            console.log(users);
+            res.render("index", {users});
+        }
+    })
+
+
+})
+// post route for adding a user
+app.post('/users', function(req, res) {
+    console.log("POST DATA", req.body);
+    var user = new User({name: req.body.name, age:req.body.age});
+    user.save(function(err){
+        if(err){
+            console.log(err);
+        }
+        else{
+            console.log("User saved!!")
+            res.redirect('/');
+        }
+    })
+})
+// tell the express app to listen on port 8192
+var server = app.listen(8192, function() {
+ console.log("Listening on port 8192");
+});
+
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function (socket) {
+    console.log("WE ARE USING SOCKETS!");
+    console.log(socket.id);
+    socket.on("button_clicked", function(data){
+
+    });
+});
