4da0478e5e8d4e82a7efe63c07faa04b2ed9c987
Nishant Aggarwal <nish8192@gmail.com>
2017-04-21 16:25:45 -0400

    Finished the MEAN Black Belt Exam

--- Full_MEAN/BASE/client/assets/controllers/userController.js
+++ Full_MEAN/BASE/client/assets/controllers/userController.js
@@ -1,11 +1,14 @@
 app.controller('userController', ["$scope", "boardFactory", "$location", "$cookies", function($scope, boardFactory, $location, $cookies){
 
+    $scope.flag = false;
     if(!$cookies.get('user_id')){
+        $scope.flag = false;
         $location.url('/');
     }
 
     $scope.logout = function(){
         $cookies.remove("user_id");
+        $scope.flag = false;
         $location.url('/');
     }
 }])
--- Full_MEAN/MEAN_Wall/server/config/mongoose.js
+++ Full_MEAN/MEAN_Wall/server/config/mongoose.js
@@ -6,7 +6,7 @@ var fs = require('fs');
 var path = require('path');
 // connect to mongoose!
 
-mongoose.connect('mongodb://localhost/MEAN_Wall'); //<-- ADD NEW PATH AFTER localhost/
+mongoose.connect('mongodb://localhost/Appointments'); //<-- ADD NEW PATH AFTER localhost/
 // create a variable that points to the path where all of the models live
 var models_path = path.join(__dirname, './../models');
 // read all of the files in the models_path and require (run) each of the javascript files
